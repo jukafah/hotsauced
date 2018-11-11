241e376fe67e95f4b77616eb1560628c162d83a4
jukafah <steve.momcilovic@gmail.com>
2018-11-11 13:32:24 -0600

    refactored contact to about, including placeholder text and fixing up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-  get 'contact', to: 'contact#index'
+  get 'about', to: 'about#index'
   get 'welcome', to: 'welcome#index'
 
   resources :sauces do
     resources :comments
   end
 
-  resources :contact
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- spec/views/about/index.html.erb_spec.rb
+++ spec/views/about/index.html.erb_spec.rb
@@ -1,20 +1,16 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'contact/index', type: :view do
-
+RSpec.describe 'about/index', type: :view do
   it 'renders page content' do
     render
     expect('.about-page-content')
   end
-  
+
   it 'renders avatar' do
     render
     expect(rendered).to have_selector('.avatar')
-  end
-
-  it 'renders contact section' do
-    render
-    expect(rendered).to have_selector('.contact-section')
   end
 
   it 'renders about section' do
@@ -22,4 +18,8 @@ RSpec.describe 'contact/index', type: :view do
     expect(rendered).to have_selector('.about-section')
   end
 
+  it 'renders contact section' do
+    render
+    expect(rendered).to have_selector('.contact-section')
+  end
 end
