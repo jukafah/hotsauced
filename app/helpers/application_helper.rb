50ad5047aab9d50c9cad7e02ba3108aeb8360d94
jukafah <steve.momcilovic@gmail.com>
2018-11-15 16:47:34 -0700

    fixed navbar, updated style, forgot boostrap requires in app.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,12 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def active?(path)
+    return 'nav-link active' if current_page?(path) else 'nav-link'
+    
+  end
 
-    def is_active?(path)
-
-        return 'nav-link active' if current_page?(path) else 'nav-link'
-    end
-
-    def page_title
-      'HOT SAUCED'
-    end
+  def page_title
+    'HOT SAUCED'
+  end
 end
