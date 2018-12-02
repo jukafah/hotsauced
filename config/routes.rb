000232d1db54c84405c561f1468c70d9fc7e2d5d
jukafah <steve.momcilovic@gmail.com>
2018-12-02 15:52:08 -0600

    clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # get 'errors/not_found'
-  # get 'errors/internal_server_error'
   match '/404', to: 'errors#not_found', via: :all
   match '500', to: 'errors#internal_server_error', via: :all
 
