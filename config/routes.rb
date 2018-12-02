cd28f9fb04401d560c03a1c15a6909b3e50a9764
jukafah <steve.momcilovic@gmail.com>
2018-12-02 15:37:32 -0600

    basic custom 404 page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get '/' => redirect('/home')
+  # get 'errors/not_found'
+  # get 'errors/internal_server_error'
+  match '/404', to: 'errors#not_found', via: :all
+  match '500', to: 'errors#internal_server_error', via: :all
 
+  get '/' => redirect('/home')
   get 'about', to: 'about#index'
   get 'contact', to: 'contact#index'
   post 'contact', to: 'contact#create'
