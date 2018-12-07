45fe1d9768a16f0c40f24e2f7a150f14ecb197a7
jukafah <steve.momcilovic@gmail.com>
2018-12-07 15:07:33 -0600

    mailer fix attempt

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
+# require
 
 class MessageMailer < ApplicationMailer
+  require 'mailgun'
+
   def contact(message)
-    Mail.deliver do
-      from message.email
-      to 'steve.momcilovic@gmail.com'
-      subject 'HOT SAUCED: Contact'
-      body message.body
-    end
+    @body = message.body
+    mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
+    message_params = {
+      from: message.email,
+      to: 'steve.momcilovic@gmail.com',
+      subject: 'Contact - Hotsauced',
+      text: message.body
+    }
+    mg_client.send_message ENV['MAILGUN_DOMAIN'], message_params
   end
 end
