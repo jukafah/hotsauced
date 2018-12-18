7f3fd6e9156d8e3242166bd0f32e176a967a9b80
jukafah <steve.momcilovic@gmail.com>
2018-12-18 13:19:50 -0600

    fix broken images

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
-require_relative 'boot'
+# frozen_string_literal: true
 
+require_relative 'boot'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,7 +12,9 @@ module Hotsauced
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.exceptions_app = self.routes
+    config.exceptions_app = routes
+
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
